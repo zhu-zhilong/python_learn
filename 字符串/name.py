a6678920b9fdbbd16916d7ce4f51f235a9fbc870
zhu-zhilong <79895517+zhu-zhilong@users.noreply.github.com>
2021-03-26 18:28:23 +0800

    班门弄斧罢了

---
+++ 字符串/name.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8-*-
+# @software   : PyCharm      
+# @file       :   name.py
+# @Time       :   2021/3/26 17:14
+'''name="ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name="ada"
+last_name="lovelace"
+full_name=first_name+" "+last_name
+print(full_name)
+print("hello, "+full_name.title()+"! ")
+print("\tpython")
+print("Languages:\nPython\nC\nJavaScript")'''
+some_str="zhu-zhilong, is my, name"
+print(some_str.split(','))
+print(some_str.count('zhu'))
+print(some_str.index("i"))
